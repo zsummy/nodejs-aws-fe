a1cfd966af11108a8b5f3a9b81b5ec7cd559fd6b
zsummy <zsummy@gmail.com>
2020-11-14 20:25:01 +0100

    Task 4 (#3)
    * feat: migrate parameters from env to cli opts
    
    * feat: remove profile
    
    * feat: integrate with POST /products api
    
    * fix: allow PublicRead to enable SPA routes

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,17 @@
-
 const API_PATHS = {
   product: 'https://axes4hauza.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev'
+  bff: 'https://axes4hauza.execute-api.eu-west-1.amazonaws.com/dev'
 };
+
+/*
+const API_PATHS = {
+  product: 'http://localhost:4000/dev',
+  order: 'http://localhost:4000/dev',
+  import: 'http://localhost:4000/dev',
+  bff: 'http://localhost:4000/dev',
+}
+*/
 
 export default API_PATHS;
--- src/models/Product.ts
+++ src/models/Product.ts
@@ -5,10 +5,16 @@ export type Product = {
   title: string,
   description: string,
   price: number,
+  count: number,
 };
 
 export const ProductSchema = Yup.object().shape({
   title: Yup.string().required(),
   description: Yup.string(),
-  price: Yup.number().required(),
+  price: Yup.number().positive().test(
+    'is-price',
+    'only two decimal digits are allowed',
+    value => /^(\d+(\.\d{1,2})?$)|(\.\d{1,2}$)/.test(`${value}`)
+  ).required(),
+  count: Yup.number().integer().min(0).required(),
 });
