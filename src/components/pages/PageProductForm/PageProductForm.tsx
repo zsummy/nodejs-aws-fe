da78325b3dcb8ee6130dc61043361e2013956811
zsummy <zsummy@gmail.com>
2020-11-14 20:25:01 +0100

    Task 4 (#3)
    * feat: migrate parameters from env to cli opts
    
    * feat: remove profile
    
    * feat: integrate with POST /products api
    
    * fix: allow PublicRead to enable SPA routes

--- src/components/pages/PageProductForm/PageProductForm.tsx
+++ src/components/pages/PageProductForm/PageProductForm.tsx
@@ -108,8 +108,10 @@ export default function PageProductForm() {
 
   const onSubmit = (values: FormikValues) => {
     const formattedValues = ProductSchema.cast(values);
+    console.log(formattedValues)
     const productToSave = id ? {...ProductSchema.cast(formattedValues), id} : formattedValues;
-    axios.put(`${API_PATHS.bff}/product`, productToSave)
+    const method = id ? axios.put : axios.post
+    method(`${API_PATHS.bff}/products`, productToSave)
       .then(() => history.push('/admin/products'));
   };
 
@@ -118,7 +120,7 @@ export default function PageProductForm() {
       setIsLoading(false);
       return;
     }
-    axios.get(`${API_PATHS.bff}/product/${id}`)
+    axios.get(`${API_PATHS.bff}/products/${id}`)
       .then(res => {
         setProduct(res.data);
         setIsLoading(false);
