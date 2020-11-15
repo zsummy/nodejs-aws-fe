4cf5f1aa7ed4b066823f140af08efa5cf24eac81
Evgenii Zhukauskas <zsummy@gmail.com>
2020-11-15 01:03:27 +0100

    feat: add upload url

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: 'https://axes4hauza.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://dxjbu07glf.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://axes4hauza.execute-api.eu-west-1.amazonaws.com/dev'
 };
 
