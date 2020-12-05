9d55b1667ed4d3b3081a43fe5b0bf9a7c5cf976e
Evgenii Zhukauskas <zsummy@gmail.com>
2020-12-05 23:03:35 +0100

    feat: apply latest patch

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,9 @@ const API_PATHS = {
   product: 'https://6e3iol5e2i.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://319e3qh7b0.execute-api.eu-west-1.amazonaws.com/dev',
-  bff: 'https://6e3iol5e2i.execute-api.eu-west-1.amazonaws.com/dev'
+  bff: 'https://6e3iol5e2i.execute-api.eu-west-1.amazonaws.com/dev',
+  // cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  cart: 'http://localhost:4000/api',
 };
 
 /*
@@ -11,6 +13,7 @@ const API_PATHS = {
   order: 'http://localhost:4000/dev',
   import: 'http://localhost:4000/dev',
   bff: 'http://localhost:4000/dev',
+  cart: 'http://localhost:4000/api',
 }
 */
 
